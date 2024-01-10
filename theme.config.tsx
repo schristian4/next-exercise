09ba43add19d43e9aadb0a4fd938e15fee8feb1f
Spencer Christian <s.christian141@gmail.com>
2024-01-09 19:43:13 -0500

    initial

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Vercel | Spencer Christian - Take Home Exercise</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/schristian4/next-exercise',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/schristian4/next-exercise',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Thank you for your consideration - Nextra',
   },
 }
 
